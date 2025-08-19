38d2357ab483006bd900e7557b58cccbac110f87
Do Phu Luan <luandophu8114@gmail.com>
2025-08-19 11:11:45 +0700

    BaiTap01

---
+++ BaiTap01/my-profile/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Xin chào, tôi là Đỗ Phú Luân</h1>
+      <p>MSSV: 22110372</p>
+      <p>Email: luandophu8114@gmail.com</p>
+    </div>
+  );
+}
+
+export default App;
